c46f38d84a8e73176e301080eb54a671afbf02b1
adlythebaud <adlythebaud@gmail.com>
2017-10-26 00:15:42 -0600

    finishing up timer class's methods.

--- Doro/Doro/Controllers/ViewController.swift
+++ Doro/Doro/Controllers/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
       // Dispose of any resources that can be recreated.
    }
    
+   // to repeat the timers, use a do-while loop.
+   
 
 }
 
--- Doro/Doro/Models/CountdownTimer.swift
+++ Doro/Doro/Models/CountdownTimer.swift
@@ -28,13 +28,12 @@ class CountdownTimer {
    var isSet: Bool
    var isPaused: Bool
    
-   // how much time is left on current timer?
-   var timeRemaining: TimeInterval?
+   
    
    
    init(workDuration: TimeInterval, breakDuration: TimeInterval, doesRepeat: Bool,
         notificationSound: Int, isRunning: Bool = false, isPaused: Bool = false,
-        isSet: Bool = false, timeRemaining: TimeInterval?, workTimer: Timer?, breakTimer: Timer?) {
+        isSet: Bool = false, workTimer: Timer?, breakTimer: Timer?) {
       self.workDuration = workDuration
       self.breakDuration = breakDuration
       self.doesRepeat = doesRepeat
@@ -42,77 +41,46 @@ class CountdownTimer {
       self.isRunning = isRunning
       self.isSet = isSet
       self.isPaused = isPaused
-      self.timeRemaining = timeRemaining
+      
       self.workTimer = workTimer
       self.breakTimer = breakTimer
    }
    
-   // start the timer.
+   // start the timer. Called with a start button.
    func start() {
-      // start the timer
-      
-      /*
-       timeInterval -> number of seconds in between firings of the timer.
-       target       -> The object to which to send the message specified by selector when timer fires
-       selector     -> message to send to target when timer fires.
-       userInfo     -> userInfo of timer. Can be nil
-       repeats      -> if true, timer will repeatedly reschedule itself until invalidated. If false,
-                       timer fires once.
-       */
-      
       
    }
    
-   // pause the timer.
+   // pause the timer. Called with a pause button.
    func pause() {
       
    }
    
-   // stop the timer.
+   // stop the timer. Called with a stop button.
    func stop() {
       
    }
    
    // start a work timer.
    func startWorkTimer() {
-      self.workTimer = Timer(timeInterval: 1, target: self, selector: #selector(self.updateTime), userInfo: nil, repeats: true)
+      self.workTimer = Timer(timeInterval: 1, target: self, selector: #selector(self.updateWorkTimer), userInfo: nil, repeats: true)
    }
    
    // start a break timer.
    func startBreakTimer() {
-      self.breakTimer = Timer(timeInterval: 1, target: self, selector: #selector(self.updateTime), userInfo: nil, repeats: true)
+      self.breakTimer = Timer(timeInterval: 1, target: self, selector: #selector(self.updateBreakTimer), userInfo: nil, repeats: true)
    }
    
-   @objc func updateTime() {
-      
+   // decrement workDuration by 1.
+   @objc func updateWorkTimer() {
+      workDuration -= 1
    }
    
-//   @objc func updateTime() {
-//      // subtract one second
-//      timeRemaining -= 1
-//
-//      // convert timeRemaing to hours:minutes:seconds
-//      hours = Int(timeRemaining) / 3600
-//      minutes = (Int(timeRemaining) % 3600) / 60
-//      seconds = (Int(timeRemaining) % 3600) % 60
-//
-//      // change timeRemainingLabel.text to reflect time change
-//      timeRemainingLabel.text = String(format: "%02d:%02d:%02d", hours!, minutes!, seconds!)
-//
-//      if timeRemaining < 1 {
-//         // timer?.fire()
-//         timer?.invalidate()
-//
-//         // play alert sound
-//         let systemSoundID: SystemSoundID = 1016
-//         // AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
-//         AudioServicesPlayAlertSound(systemSoundID)
-//         isTimerRunning = false
-//
-//         // update the view
-//         updateView()
-//      }
-//   }
+   // decrement breakDuration by 1.
+   @objc func updateBreakTimer() {
+      breakDuration -= 1
+   }
+
    
 //   func updateView() {
 //
@@ -122,9 +90,9 @@ class CountdownTimer {
 //      timeRemaining = 5
 //
 //      // convert TimeRemaining to hours:minutes:seconds for initial countdown
-//      hours = Int(timeRemaining) / 3600
-//      minutes = (Int(timeRemaining) % 3600) / 60
-//      seconds = (Int(timeRemaining) % 3600) % 60
+//      var hours = Int(timeRemaining) / 3600
+//      var minutes = (Int(timeRemaining) % 3600) / 60
+//      var seconds = (Int(timeRemaining) % 3600) % 60
 //
 //      // show the correct TimeRemaining
 //      timeRemainingLabel.text = String(format: "%02d:%02d:%02d", hours!, minutes!, seconds!)
